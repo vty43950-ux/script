d70d8f1abe7c140e39304d653cc16f51fa754731
vty43950-ux <vty43950@gmail.com>
2026-03-17 19:59:38 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -6,7 +6,7 @@ local CoreGui = game:GetService("CoreGui")
 local LocalPlayer = Players.LocalPlayer
 
 -- API URL CỦA BẠN 
-local API_URL = "https://zenithghz.qzz.io/api/update"
+local API_URL = "https://zenithghz.indevs.in/api/update"
 
 -------------------------------------------------------------------------------
 -- GIAO DIỆN (UI) THÔNG BÁO CHO NGƯỜI DÙNG
@@ -367,4 +367,5 @@ task.spawn(function()
         postDataToAPI()
     end
 end)
+
 
