86a275b52a24335e8c1859fd1be1b4aca13b2927
vty43950-ux <vty43950@gmail.com>
2026-03-26 22:08:25 +0700

    Update Stockbot.lua

--- Stockbot.lua
+++ Stockbot.lua
@@ -11,7 +11,7 @@ type table = {
 _G.Configuration = {
 	--// Reporting
 	["Enabled"] = true,
-	["Webhook"] = "https://discord.com/api/webhooks/1482391815024803963/6V8VLwhL7X1o9FL_n1GNxxsoRH6su1tDzhbxzT4wJe_qr_MGCVaqp1fUs8ZKdnbyyC_H.....", -- replace with your webhook url
+	["Webhook"] = "https://discord.com/api/webhooks/1482391815024803963/6V8VLwhL7X1o9FL_n1GNxxsoRH6su1tDzhbxzT4wJe_qr_MGCVaqp1fUs8ZKdnbyyC_H", -- replace with your webhook url
 	["Weather Reporting"] = true,
 	
 	--// User
